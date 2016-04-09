306b297005fe1f51ba93dc00a0e3859331059e9a
matthewlim <limmmmm@gmail.com>
2016-04-09 16:21:01 -0400

    Cleanup.

--- app/src/main/java/tenkstudio/makingpaper/MainActivity.java
+++ app/src/main/java/tenkstudio/makingpaper/MainActivity.java
@@ -1,16 +1,35 @@
 package tenkstudio.makingpaper;
 
-import android.content.Intent;
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import tenkstudio.makingpaper.view.TimerAnimationView;
+
 public class MainActivity extends AppCompatActivity {
+
+    private TimerAnimationView view;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        startActivity(new Intent(this, TimerAnimatorActivity.class));
+        view = (TimerAnimationView) findViewById(R.id.timer_view);
+    }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (view != null) {
+            view.onActivityResume();
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (view != null) {
+            view.onActivityPause();
+        }
     }
 }
